1a7833e70cb5439924652b820a6bb58b219a19ae
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-21 17:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function destructivelyRemoveLastKitten(name) {
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name)
 }
+
+function appendKitten(name) {
+  kittens.appendKitten()
+}
